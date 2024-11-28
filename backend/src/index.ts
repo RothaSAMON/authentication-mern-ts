f967ce4b8d3ed74543acd1a537eb23f364b0a090
Rotha SAMON <samonrotha@gmail.com>
2024-11-28 18:40:03 +0700

    Added controller middleware route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,37 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import connectToDatabase from "./config/db";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
+import cookieParser from "cookie-parser";
+import errorHandler from "./middleware/errorHandler";
+import catchErrors from "./utils/catchErrors";
+import { OK } from "./constants/http";
+import authRoutes from "./routes/auth.route";
 
 const app = express();
 
-app.listen(4004, () => {
-  console.log("Server is running on port 4004 in dev!🚀");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: APP_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.get("/", (req, res, next) => {
+  res.status(OK).json({
+    status: "Healthy",
+  });
+});
+
+app.use("/auth", authRoutes);
+
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT} in ${NODE_ENV}!🚀`);
+  await connectToDatabase();
 });
